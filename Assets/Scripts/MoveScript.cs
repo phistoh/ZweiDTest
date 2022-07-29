d7937efea881d06b0122c0bf0990be30b74a3abd
Gobbert <gobbert@uni-trier.de>
2022-07-29 12:11:48 +0200

    Player can shoot

---
+++ Assets/Scripts/MoveScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+	public Vector2 speed = new Vector2(10, 10);
+
+    public Vector2 direction = new Vector2(-1, 0);
+
+    private Vector2 movement;
+    private Rigidbody2D rigidbodyComponent;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		 movement = new Vector2(
+			speed.x * direction.x,
+			speed.y * direction.y
+		);
+    }
+	
+	void FixedUpdate()
+	{
+		if(rigidbodyComponent == null) rigidbodyComponent = GetComponent<Rigidbody2D>();
+		
+		rigidbodyComponent.velocity = movement;
+	}
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+	
+	public Vector2 speed = new Vector2(50, 50);
+	
+	private Vector2 movement;
+	private Rigidbody2D rigidbodyComponent;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+		
+		movement = new Vector2(
+			speed.x * inputX,
+			speed.y * inputY
+		);
+		
+		bool shoot = Input.GetButton("Fire1");
+		shoot |= Input.GetButton("Fire2");
+		
+		if (shoot)
+		{
+			WeaponScript weapon = GetComponent<WeaponScript>();
+			if (weapon != null)
+			{
+				weapon.Attack(false);
+			}
+		}
+    }
+	
+	void FixedUpdate()
+	{
+		if(rigidbodyComponent == null) rigidbodyComponent = GetComponent<Rigidbody2D>();
+		
+		rigidbodyComponent.velocity = movement;
+	}
+}
